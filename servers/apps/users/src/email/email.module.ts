731e22cb171dd9f4b900556007b252fd081857fd
deo@11 <pathakdeo11@gmail.com>
2025-02-20 11:35:41 +0530

    first commit

---
+++ servers/apps/users/src/email/email.module.ts
@@ -0,0 +1,38 @@
+import { MailerModule } from '@nestjs-modules/mailer';
+import { Global, Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { EmailService } from './email.service';
+import { join } from 'path';
+import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
+
+@Global()
+@Module({
+  imports: [
+    MailerModule.forRootAsync({
+      useFactory: async (config: ConfigService) => ({
+        transport: {
+          host: config.get('SMTP_HOST'),
+          secure: true,
+          auth: {
+            user: config.get('SMTP_MAIL'),
+            pass: config.get('SMTP_PASSWORD'),
+          },
+        },
+        defaults: {
+          from: 'Deo',
+        },
+        template: {
+          dir: join(__dirname, '../../../../email-templates'),
+          adapter: new EjsAdapter(),
+          options: {
+            strict: false,
+          },
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [EmailService],
+  exports: [EmailService],
+})
+export class EmailModule {}
